0797d343a070b9f4499366887b9f1df351c334f4
Sahil Jain <sahil.2441@gmail.com>
2015-09-14 02:12:22 -0400

    Given a binary tree, find its maximum depth.

---
+++ src/MaximumDepth.java
@@ -0,0 +1,14 @@
+
+public class MaximumDepth {
+
+	public static void main(String[] args) {
+		TreeNode root=new TreeNode(10);
+		root.left=new TreeNode(20);
+		root.right=new TreeNode(30);
+		System.out.println(maxDepth(root));
+	}
+    public static int maxDepth(TreeNode root) {
+    	if(root==null)return 0;    	
+    	return Math.max(maxDepth(root.left),maxDepth(root.right))+1;  
+    }
+}
