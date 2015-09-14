c5c0d24b0e5db03906241b21048194beced847fc
Sahil Jain <sahil.2441@gmail.com>
2015-09-14 02:00:14 -0400

    Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.

---
+++ src/DeleteNode.java
@@ -0,0 +1,37 @@
+
+public class DeleteNode {
+
+	public static void main(String[] args) {
+		ListNode head=new ListNode(1);
+		head.next=new ListNode(2);
+		head.next.next=new ListNode(3);
+		head.next.next.next=new ListNode(4);
+		head.next.next.next.next=null;
+		ListNode reference=head.next.next;
+		
+		printList(head);
+		deleteNode(reference);
+		System.out.println();
+		printList(head);
+
+	}
+
+	private static void printList(ListNode node) {
+		while(node!=null){
+			System.out.print(node.val+"->");
+			node=node.next;
+		}
+	}
+
+	private static void deleteNode(ListNode node) {
+		if(node!=null && node.next!=null){
+			while(node.next.next!=null){
+				node.val=node.next.val;
+				node=node.next;
+			}
+			node.val=node.next.val;
+			node.next=null;
+		}		
+	}
+
+}
