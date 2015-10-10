cf2528faed16d8fe2a467504ea28aa1fe4283446
Sahil Jain <sahil.2441@gmail.com>
2015-10-10 04:19:21 -0400

    Reverse String.

---
+++ src/ReverseString.java
@@ -0,0 +1,26 @@
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String s = "  a ";
+		System.out.println(reverseString(s));
+	}
+
+	private static String reverseString(String s) {
+		String[] parts = s.split("\\s");
+		StringBuilder builder = new StringBuilder();
+		if (parts.length > 0) {
+			builder.append(parts[parts.length - 1]);
+			for (int j = parts.length - 2; j >= 0; j--) {
+				if (parts[j].equals("")) {
+					continue;
+				}
+				builder.append(" " + parts[j]);
+			}
+		}
+
+		return builder.toString();
+
+	}
+
+}
