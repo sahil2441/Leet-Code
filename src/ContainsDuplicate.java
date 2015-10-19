e5a533ae2e24ec876cba42280b6cb64630eaebde
Sahil Jain <sahil.2441@gmail.com>
2015-10-19 15:36:53 -0400

    Contains Duplicate

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicate {
+	public boolean containsDuplicate(int[] nums) {
+
+		if (nums.length <= 1) {
+			return false;
+		}
+
+		Map<Integer, Boolean> map = new HashMap();
+
+		for (int i = 0; i < nums.length; i++) {
+			if (map.get(nums[i]) == null) {
+				map.put(nums[i], true);
+			} else {
+				return true;
+			}
+		}
+		return false;
+
+	}
+}
