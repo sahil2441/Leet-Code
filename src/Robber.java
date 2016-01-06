3eaa62abb84a17f92df9f11d67dc11e821067e35
Sahil Jain <sahil.2441@gmail.com>
2016-01-06 16:45:22 -0500

    robber

--- src/Robber.java
+++ src/Robber.java
@@ -1,7 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Robber {
 	public static void main(String[] args) {
 		int[] arr = { 1, 3, 1, 3, 100 };
 		// System.out.println(rob(arr));
+		int[] cand = { 1, 2 };
+		int target = 4;
+		System.out.println(combinationSum2(cand, target));
+	}
+
+	public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+
+		Arrays.sort(candidates);
+		List<List<Integer>> list = new ArrayList();
+		if (target == 0)
+			return list;
+
+		List<Integer> currentList = new ArrayList();
+		helper(candidates, 0, target, currentList, list);
+		return list;
+	}
+
+	static void helper(int[] candidates, int index, int target, List<Integer> currentList, List<List<Integer>> list) {
+		if (target == 0) {
+			list.add(new ArrayList(currentList));
+			return;
+		}
+		if (target < 0)
+			return;
+		for (int i = index; i < candidates.length; i++) {
+			if (i > index && candidates[i] == candidates[i - 1])
+				continue;
+			currentList.add(currentList.size(), candidates[i]);
+			helper(candidates, index + 1, target - candidates[i], currentList, list);
+			currentList.remove(currentList.size() - 1);
+		}
+
 	}
 
 	public static int rob(int[] nums) {
@@ -13,6 +49,7 @@ public class Robber {
 		if (nums.length == 2)
 			return Math.max(nums[0], nums[1]);
 		return Math.max(robHelper(nums, 0, nums.length - 2), robHelper(nums, 1, nums.length - 1));
+
 	}
 
 	public static int robHelper(int[] nums, int left, int right) {
